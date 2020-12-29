4fd4dc7557a1db76ba859466e5e65cc566eb2af7
Vepnar <agdehaan@gmail.com>
2020-12-29 14:05:51 +0100

    Run polybar on every monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,4 +7,11 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 interface=$(ip route | grep '^default' | awk '{print $5}' | head -n1)
 
-polybar -c ~/.config/polybar/config.ini main &
+# Polybar run on every monitor
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -c ~/.config/polybar/config.ini main &
+  done
+else
+  polybar -c ~/.config/polybar/config.ini main &
+fi
